f04e7ddd60dd4d3eeddf37c15749ff50bbe48e6b
RachitSinghh <singhrachit@outlook.in>
2025-03-23 19:16:55 +0530

    added login route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,12 @@
 import express, { Router } from "express";
 import User from "../models/User.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-
-const generateToken = (userId) =>{
-  return  jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '15d'})
-}
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
 
 router.post("/register", async (req, res) => {
   try {
@@ -55,25 +54,54 @@ router.post("/register", async (req, res) => {
 
     await user.save();
 
-    const token = generateToken(user._id); 
+    const token = generateToken(user._id);
 
     res.status(201).json({
       token,
       user: {
         _id: user._id,
-        username: user.username, 
-        email:user.email,
-        profileImage: user.profileImage
-      }
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
     });
   } catch (error) {
     console.log("Error in register route", error);
-    res.status(500).json({message: "Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 router.post("/login", async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    // check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // check if password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      token,
+      user:{
+        id: user._id, 
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage, 
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({message: "Internal server error"})
+  }
 });
 
 export default router;

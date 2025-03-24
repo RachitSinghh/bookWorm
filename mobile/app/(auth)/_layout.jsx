72de1586d8d2c4faec168cc42be1267ef07d83a7
RachitSinghh <singhrachit@outlook.in>
2025-03-24 19:31:32 +0530

    1. Added Authentication layout and screens. Implement Safe area context and navigation links

---
+++ mobile/app/(auth)/_layout.jsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native'
+import { Stack } from 'expo-router'
+import React from 'react'
+
+export default function AuthLayout() {
+  return <Stack screenOptions={{headerShown:false}}>
+  </Stack>
+}
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import {Link} from "expo-router"; 
+import { Text, View,StyleSheet } from "react-native";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>123</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>123</Text>
+      <Image />
+      <Link href="/(auth)/signup">Signup </Link>
+      <Link href="/(auth)">Login </Link>
     </View>
   );
 }
+
+const styles =StyleSheet.create({
+  container:{
+    flex: 1, 
+    justifyContent: "center", 
+    "alignItems": "center", 
+  },
+  title:{
+    color: "blue", 
+  }
+})

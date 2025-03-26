197d87aa56f8fb89b6eabb732f32848b5b37ec21
RachitSinghh <singhrachit@outlook.in>
2025-03-27 00:22:43 +0530

    Removed accidental submodule bookWorm

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,14 @@ import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+import job from "./lib/cron.js"
 
 import { connectDB } from "./lib/db.js";
 
 const app = express(); 
 const PORT = process.env.PORT || 3000
 
-
+job.start();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes)

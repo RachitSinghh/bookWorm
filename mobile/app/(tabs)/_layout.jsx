895f8d073d6408db7c3f034b2f5942d664b001e4
RachitSinghh <singhrachit@outlook.in>
2025-03-28 14:07:34 +0530

    1.Enhances tab navigation with icons and dynamic styles 2. Implement authentication check and redirect logic

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,13 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets(); 
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="create" />
-      <Tabs.Screen name="profile" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary, 
+        headerTitleSytle:{
+          color: COLORS.textPrimary,
+          fontWeight: "600"
+        },
+        headerShadowVisible: false, 
+        tabBarStyle:{
+          backgroundColor: COLORS.cardBackground, 
+          borderTopWidth: 1, 
+          borderTopColor: COLORS.border, 
+          padding: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom, 
+          
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
